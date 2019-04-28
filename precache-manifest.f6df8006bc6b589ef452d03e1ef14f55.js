0939accf8414f175cd40ea1e43bde63de8eed055
Fernando Oviedo <fernando.n.oviedo@gmail.com>
2019-04-28 20:01:03 -0300

    Updates

---
+++ precache-manifest.f6df8006bc6b589ef452d03e1ef14f55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a816ad9e88cb889d960",
+    "url": "/notes-rjs/static/js/runtime~main.e2d24860.js"
+  },
+  {
+    "revision": "fb774a09450a0c0c640f",
+    "url": "/notes-rjs/static/js/main.5f94cbaf.chunk.js"
+  },
+  {
+    "revision": "32fd9a82221f97af75d9",
+    "url": "/notes-rjs/static/js/2.e335ab5f.chunk.js"
+  },
+  {
+    "revision": "fb774a09450a0c0c640f",
+    "url": "/notes-rjs/static/css/main.e4220d31.chunk.css"
+  },
+  {
+    "revision": "43283e407563eaf845ea5d5e8194ec85",
+    "url": "/notes-rjs/index.html"
+  }
+];
